46d5d09be54e738a453539af6ad79d5c75e56d30
gettinToasty <sean.r.beyer@gmail.com>
2016-12-08 13:59:57 -0800

    Feature complete RecordLablr

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,12 @@
 module TracksHelper
+  include ERB::Util
+
+  def ugly_lyrics(lyrics)
+    html = ""
+    lyrics.split("\n").each do |line|
+      html += "<pre>&#9835 #{h(line)}</pre>"
+    end
+    html.html_safe
+  end
+
 end
