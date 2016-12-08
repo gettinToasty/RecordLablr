47be1206395e9245c730350b087dce618b1cb3a0
gettinToasty <sean.r.beyer@gmail.com>
2016-12-08 12:10:03 -0800

    Add Note model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  
+
   validates :username,
             :email,
             :password_digest,
@@ -8,6 +8,8 @@ class User < ApplicationRecord
 
   validates :username, :email, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
+
+  has_many :notes
 
   after_initialize :ensure_session_token
 
